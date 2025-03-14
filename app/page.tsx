64b02ec23cf80c8b39cd04c0f77e0811a0eba7b7
shiori.sato <jtqbk3670@gmail.com>
2025-03-14 10:53:10 +0900

    Suspenseでローディング機能実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 "use client"; //クライアントコンポーネント
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import Image from "next/image";
 
 export default function PokeCatch() {
   const [pokemon, setPokemon] = useState<{ name: string; imageUrl:string } | null>(null);
-  const fetchRandomPokemon = async () => {
-    const pokemonId = Math.floor(Math.random() * 898) + 1;
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+  const [isPending, startTransition] = useTransition();
 
-    if (response.ok) {
-      const data = await response.json();
-      setPokemon({
-          name: data.name.toUpperCase(),
-          imageUrl: data.sprites.front_default,
-        });
-      }else {
-        setPokemon(null);
-      }
+  const fetchRandomPokemon = async () => {
+    startTransition(async () => {
+      const pokemonId = Math.floor(Math.random() * 898) + 1;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+
+      if (response.ok) {
+        const data = await response.json();
+        setPokemon({
+            name: data.name.toUpperCase(),
+            imageUrl: data.sprites.front_default,
+          });
+        }else {
+          setPokemon(null);
+        }
+    });
   };
   
   return (
@@ -25,7 +29,10 @@ export default function PokeCatch() {
       <header className="fixed top-0 w-full bg-yellow-300 py-4 text-neutral-900 text-center text-lg font-bold">
         ポケモンずかん
       </header>
-      {pokemon ? (
+
+      {isPending ? (
+        <p className="text-lg mt-4 text-neutral-900">Loading...</p>
+      ) : pokemon ? (
         <div className="flex flex-col items-center mt-8">
           <Image src={pokemon.imageUrl} alt={pokemon.name} width={150} height={150} />
           <p className="text-xl text-neutral-900 font-bold mt-4">{pokemon.name}</p>
@@ -33,11 +40,13 @@ export default function PokeCatch() {
       ) : (
         <p className="text-lg mt-4 text-neutral-900">ボタンを押してポケモンをゲット！</p>
       )}
+
       <button 
         className="mt-3 px-4 py-1 bg-yellow-500 text-neutral-900 text-lg font-bold rounded hover:bg-yellow-300 transition-all duration-200 delay-200 shadow"
         onClick={fetchRandomPokemon}
+        disabled={isPending}
       >
-        ゲットだぜ！
+        {isPending ? " _(:3」 ∠)_ " : "ゲットだぜ！"}
       </button>
     </div>
   );
